483842d0e103d5593a95c534555043f8ce6f5727
Ho Wing Yip <sploooosh884@gmail.com>
2024-11-17 06:22:04 +0800

    Added album art retrieval

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::{process::Command, thread, time::{Duration, SystemTime, UNIX_EPOCH}};
+use std::{collections::HashMap, process::Command, sync::{LazyLock, Mutex}, thread, time::{Duration, SystemTime, UNIX_EPOCH}};
 
-use discord_rich_presence::{activity::{Activity, ActivityType, Timestamps}, DiscordIpc, DiscordIpcClient};
+use discord_rich_presence::{activity::{Activity, ActivityType, Assets, Timestamps}, DiscordIpc, DiscordIpcClient};
 
 #[derive(Debug)]
 struct CmusStatus {
@@ -49,13 +49,72 @@ fn unix_epoch_secs_from_now(secs: &i64) -> i64 {
   i64::try_from(SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs()).unwrap() + secs
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn get_album_art_url(
+  reqwest_client: &reqwest::Client, album: &String, artist: &String,
+) -> Result<String, Box<dyn std::error::Error>> {
+  if album.is_empty() || artist.is_empty() {
+    return Err("album and artist must not be empty".into());
+  }
+
+  static ALBUM_ART_CACHE: LazyLock<Mutex<HashMap<(String, String), String>>> = LazyLock::new(|| Mutex::new(HashMap::new()));
+
+  let first_artist = match artist.split_once(",") {
+    Some((first_artist, _)) => first_artist,
+    None => artist,
+  };
+
+  let cache_key = (first_artist.to_owned(), album.to_owned());
+  match ALBUM_ART_CACHE.lock()?.get(&cache_key) {
+    Some(url) => return Ok(url.to_owned()),
+    None => {
+      println!("Cache miss for key {:?}", cache_key);
+    },
+  }
+
+  let response = reqwest_client.post("https://itunes.apple.com/search")
+    .form(&[
+      ("term", format!("{} {}", first_artist, album)),
+      ("entity", "album".to_owned()),
+    ])
+    .send()
+    .await?
+    .json::<serde_json::Value>()
+    .await?;
+
+  let url = "https://a5.mzstatic.com/us/r1000/0/".to_string() +
+    &response
+      .get("results").ok_or("'results' key missing from response")?
+      .as_array().ok_or("'results' field could not be converted to Vec")?
+      .iter()
+      .find(|v| -> bool {
+        match v.get("collectionName") {
+          Some(found_album) => found_album == album,
+          None => false,
+        }
+      }).ok_or("No album found with matching name")?
+      .get("artworkUrl100").ok_or("'artworkUrl100' key missing from 'results' object")?
+      .to_string()
+      .splitn(2, "/image/thumb")
+      .collect::<Vec<&str>>()[1]
+      .rsplitn(2, "/")
+      .collect::<Vec<&str>>()[1]
+      .to_owned();
+  
+  ALBUM_ART_CACHE.lock()?.insert(cache_key, url.clone());
+
+  Ok(url)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
   println!("Connecting to Discord...");
 
-  let mut client = DiscordIpcClient::new("1307075307299405844")?;
-  client.connect()?;
+  let mut discord_ipc_client = DiscordIpcClient::new("1307075307299405844")?;
+  discord_ipc_client.connect()?;
 
   println!("Successfully connected to Discord.");
+
+  let reqwest_client = reqwest::Client::new();
 
   loop {
     let cmus_remote_result = Command::new("cmus-remote")
@@ -64,28 +123,29 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     match parse_status_str(&String::from_utf8_lossy(&cmus_remote_result.stdout)) {
       Ok(CmusStatus { playing, title, artist, album, duration_sec, position_sec }) => {
-        // TODO: add album art using iTunes API
-        // https://github.com/bendodson/itunes-artwork-finder/blob/master/api.php
-        // Leave album art blank if album is empty.
-
         if playing {
-          client.set_activity(Activity::new()
+          discord_ipc_client.set_activity(Activity::new()
             .activity_type(ActivityType::Listening)
             .details(&title)
             .state(&artist)
             .timestamps(Timestamps::new()
               .start(unix_epoch_secs_from_now(&-position_sec.try_into()?))
               .end(unix_epoch_secs_from_now(&(duration_sec - position_sec).try_into()?)))
+            .assets(Assets::new()
+              .large_image(match &get_album_art_url(&reqwest_client, &album, &artist).await {
+                Ok(album_art_url) => album_art_url,
+                Err(_) => "",
+              }))
           )?;
         } else {
-          client.clear_activity()?;
+          discord_ipc_client.clear_activity()?;
         }
       },
 
       Err(e) => {
         println!("An error occurred while parsing cmus-remote output: {:?}", e);
         println!("Clearing Rich Presence status.");
-        client.clear_activity()?;
+        discord_ipc_client.clear_activity()?;
       }
     }
 
