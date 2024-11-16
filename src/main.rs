aaa2c2159d03659bc04c536bbd111952ebf997b0
Ho Wing Yip <sploooosh884@gmail.com>
2024-11-16 23:09:13 +0800

    Improved error handling + clear status if paused

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use std::{collections::HashMap, process::{Command, Output}, ptr, thread, time::{Duration, SystemTime, UNIX_EPOCH}};
+use std::{process::Command, thread, time::{Duration, SystemTime, UNIX_EPOCH}};
 
-use discord_rich_presence::{activity::{self, Activity, ActivityType, Timestamps}, DiscordIpc, DiscordIpcClient};
+use discord_rich_presence::{activity::{Activity, ActivityType, Timestamps}, DiscordIpc, DiscordIpcClient};
 
 #[derive(Debug)]
 struct CmusStatus {
+  playing: bool,
   title: String,
   artist: String,
   album: String,
@@ -11,7 +12,8 @@ struct CmusStatus {
   position_sec: u32,
 }
 
-fn parse_status_str(status_str: &str) -> CmusStatus {
+fn parse_status_str(status_str: &str) -> Result<CmusStatus, Box<dyn std::error::Error>> {
+  let mut status: String = Default::default();
   let mut title: String = Default::default();
   let mut artist: String = Default::default();
   let mut album: String = Default::default();
@@ -19,6 +21,7 @@ fn parse_status_str(status_str: &str) -> CmusStatus {
   let mut position_sec: String = Default::default();
 
   let mut string_to_field = [
+    ("status ", &mut status),
     ("tag title ", &mut title),
     ("tag artist ", &mut artist),
     ("tag album ", &mut album),
@@ -34,11 +37,12 @@ fn parse_status_str(status_str: &str) -> CmusStatus {
     }
   }
 
-  CmusStatus {
+  Ok(CmusStatus {
+    playing: status == "playing",
     title, artist, album,
-    duration_sec: duration_sec.parse().unwrap(),
-    position_sec: position_sec.parse().unwrap(),
-  }
+    duration_sec: duration_sec.parse()?,
+    position_sec: position_sec.parse()?,
+  })
 }
 
 fn unix_epoch_secs_from_now(secs: &i64) -> i64 {
@@ -49,33 +53,40 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
   println!("Connecting to Discord...");
 
   let mut client = DiscordIpcClient::new("1307075307299405844")?;
+  client.connect()?;
 
-  match client.connect() {
-    Ok(()) => println!("Successfully connected to Discord."),
-    Err(e) => println!("Error occurred while connecting to Discord: {}", &e),
-  }
+  println!("Successfully connected to Discord.");
 
   loop {
     let cmus_remote_result = Command::new("cmus-remote")
       .args(["-C", "status"])
-      .output();
+      .output()?;
 
-    match cmus_remote_result {
-      Ok(Output { stdout, .. }) => {
-        let CmusStatus { title, artist, album, duration_sec, position_sec } =
-          parse_status_str(&String::from_utf8_lossy(&stdout));
+    match parse_status_str(&String::from_utf8_lossy(&cmus_remote_result.stdout)) {
+      Ok(CmusStatus { playing, title, artist, album, duration_sec, position_sec }) => {
+        // TODO: add album art using iTunes API
+        // https://github.com/bendodson/itunes-artwork-finder/blob/master/api.php
+        // Leave album art blank if album is empty.
 
-        client.set_activity(Activity::new()
-          .activity_type(ActivityType::Listening)
-          .details(&title)
-          .state(&artist)
-          .timestamps(Timestamps::new()
-            .start(unix_epoch_secs_from_now(&-position_sec.try_into()?))
-            .end(unix_epoch_secs_from_now(&(duration_sec - position_sec).try_into()?)))
-        )?;
+        if playing {
+          client.set_activity(Activity::new()
+            .activity_type(ActivityType::Listening)
+            .details(&title)
+            .state(&artist)
+            .timestamps(Timestamps::new()
+              .start(unix_epoch_secs_from_now(&-position_sec.try_into()?))
+              .end(unix_epoch_secs_from_now(&(duration_sec - position_sec).try_into()?)))
+          )?;
+        } else {
+          client.clear_activity()?;
+        }
       },
 
-      Err(_) => println!("An error occurred while launching cmus-remote."),
+      Err(e) => {
+        println!("An error occurred while parsing cmus-remote output: {:?}", e);
+        println!("Clearing Rich Presence status.");
+        client.clear_activity()?;
+      }
     }
 
     thread::sleep(Duration::from_millis(1000));
